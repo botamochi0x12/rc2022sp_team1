ddbfc9f1b77bb42e51f550df1571e9955cd3dcf8
botamochi0x12 <botamochi0x12@gmail.com>
2022-03-10 20:15:30 +0900

    :art: Format indentation and spacing of Ending

--- src/directors/ending_director.rb
+++ src/directors/ending_director.rb
@@ -1,34 +1,44 @@
 require_relative 'base'
 
 module Directors
-	# エンディング画面用ディレクター
-	class EndingDirector < Base
+    # エンディング画面用ディレクター
+    class EndingDirector < Base
         attr_accessor :score
-		# 初期化
-		def initialize(screen_width:, screen_height:, renderer:)
-			super
+
+        # 初期化
+        def initialize(screen_width:, screen_height:, renderer:)
+            super
+
             self.score = 0
 
-			# テキスト表示用パネルを生成し、カメラから程よい距離に配置する
-			@description = AnimatedPanel.new(width: 1, height: 0.25, start_frame: 15, map: TextureFactory.create_ending_description)
-			@description.mesh.position.z = -0.5
-			self.scene.add(@description.mesh)
-		end
+            # テキスト表示用パネルを生成し、カメラから程よい距離に配置する
+            @description = AnimatedPanel.new(
+                width: 1,
+                height: 0.25,
+                start_frame: 15,
+                map: TextureFactory.create_ending_description
+            )
+            @description.mesh.position.z = -0.5
+            self.scene.add(@description.mesh)
+        end
 
-		# 1フレーム分の進行処理
-		def play
-			# テキスト表示用パネルを1フレーム分アニメーションさせる
-			@description.play
-		end
+        # 1フレーム分の進行処理
+        def play
+            # テキスト表示用パネルを1フレーム分アニメーションさせる
+            @description.play
+        end
 
-		# キー押下（単発）時のハンドリング
-		def on_key_pressed(glfw_key:)
-			case glfw_key
-				# ESCキー押下で終了する
-				when GLFW_KEY_ESCAPE
-					puts "クリア!!"
-					transition_to_next_director # self.next_directorがセットされていないのでメインループが終わる
-			end
-		end
-	end
+        # キー押下（単発）時のハンドリング
+        def on_key_pressed(glfw_key:)
+            case glfw_key
+                # ESCキー押下で終了する
+            when GLFW_KEY_ESCAPE
+                puts "クリア!!! Score: #{self.score}"
+                transition_to_next_director
+                # NOTE:
+                # self.next_directorがセットされていないので
+                # メインループが終わる
+            end
+        end
+    end
 end
